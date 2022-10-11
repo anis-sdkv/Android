28142871d0f4a01bd2e3e502f91288d2d6ba8a62
Anis Sadykov <sadikowas@yandex.ru>
2022-10-11 21:53:21 +0300

    add homeworks template

---
+++ Homeworks/app/src/main/java/com/example/homeworks/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.homeworks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.homeworks.Fragments.FirstFragment
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    val fragmentsContainerId: Int = R.id.main_fragments_container
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initFragments()
+    }
+
+    private fun initFragments() {
+        supportFragmentManager.beginTransaction().add(
+            fragmentsContainerId,
+            FirstFragment.getInstance(),
+            FirstFragment.FIRST_FRAGMENT_TAG
+        ).commit()
+    }
+
+    fun replaceFragment(fragment: Fragment, tag: String, detachCurrent: Boolean = false) {
+        val transaction = supportFragmentManager.beginTransaction()
+
+        if (detachCurrent) {
+            supportFragmentManager.findFragmentById(fragmentsContainerId)?.let { currentFragment ->
+                transaction.detach(currentFragment)
+            }
+        }
+
+        transaction
+            .replace(fragmentsContainerId, fragment, tag)
+            .addToBackStack(null)
+            .commit()
+    }
+}
